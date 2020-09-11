675c5e04210346fd2b35a771c0aa7e48ca95383f
xyz <xyzzhybz@163.com>
2020-09-11 20:59:27 +0800

    [xyz]

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = ({data,column}) => {
+    let output = [];
+    data.map(it => {
+        let item = {};
+        column.map(key => {
+            item[key.name] = it.shift()
+        })
+        output.push(item);
+    })
+
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+    
+    sections.sort((a,b) => {a.index - b.index}).forEach((section, index) => {
+        items.splice(section.index + index, 0, section.content)
+    });
+    return items;
+}
 export { inject };
